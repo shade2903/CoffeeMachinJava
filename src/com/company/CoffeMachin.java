27dee0eb74433119b10313f769fcc4a27f84002e
shade2903 <nighshade2903@gmail.com>
2021-04-16 12:50:44 +0300

    First commit

---
+++ src/com/company/CoffeMachin.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class CoffeMachin {
+    double watter = 2;
+    double cofee = 1;
+    public void makeCoffe(){
+       if((watter-0.5)>0 && (cofee-0.2)>0){
+           watter = watter-0.5;
+           cofee = cofee - 0.2;
+           System.out.println("Ваш кофе готов");
+       } else if((watter-0.5)<=0){
+           System.out.println("Нехватает воды.Заполните бак водой");
+       } else if((cofee-0.2)<=0) {
+           System.out.println("Нехватает кофе.Заполните бак кофе");
+       }
+
+    }
+    public double fillWateer(){
+       return watter =2 ;
+    }
+    public double fillUpCofee(){
+        return cofee =1 ;
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CoffeMachin cm = new CoffeMachin();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Выберите марку машины:");
+        System.out.println("1.Samsung");
+        System.out.println("2.Filips");
+        System.out.println("3.Redmond");
+        int brand = in.nextInt();
+
+        int n =0;
+        while (n !=8){
+            System.out.println("1. Показать текущий объем воды и кофе");
+            System.out.println("2. Приготовить Американо");
+            System.out.println("3. Приготовить Эспрессо");
+            System.out.println("4. Приготовить Двойной эспрессо");
+            System.out.println("5. Заполнить бак водой");
+            System.out.println("6. Заполнить бак водой");
+            System.out.println("7. Заполнить бак водой");
+            System.out.println("8. Заполнить бак водой");
+
+            n = in.nextInt();
+            switch (n){
+                case 1:
+                    System.out.println("Вода: " +cm.watter + " Кофе: " + cm.cofee);
+                    break;
+                case 2:
+                    // делаем американо
+                    break;
+                case 3:
+                    // делаем экспрессо
+                    break;
+                case 4:
+                    // делаем двойной экспрессо
+                    break;
+                case 5:
+                    // заполняем бак водой
+                    break;
+                case 6:
+                    // заполняем бак кофе
+                    break;
+                case 7:
+                    // иформация
+                    System.out.println("Производитель: "+cm.getClass()+);
+                    break;
+                case 8:
+                    // выключить кофе машину
+                    System.out.println("Машина выключена1");
+                    break;
+            }
+        }
+
+
+    }
+
+
+}
+
